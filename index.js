0e3a6614a4921ea8eec626e97923d5f40877a8f3
Kchou <K.chousos@protonmail.com>
2023-07-12 17:06:55 +0300

    3.8

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(morgan('tiny'))
 app.use(express.json())
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
