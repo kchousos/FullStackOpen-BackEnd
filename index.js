186a8c78ab6cf069d2dc6e4b0b12230ecba4a94a
Kchou <K.chousos@protonmail.com>
2023-07-12 18:33:59 +0300

    uibuild

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json())
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     { 
