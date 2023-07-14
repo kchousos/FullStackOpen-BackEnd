854e368a41353a512e26f027bda51d923f1aa83c
Kchou <K.chousos@protonmail.com>
2023-07-14 03:01:53 +0300

    added validation to update operations

--- index.js
+++ index.js
@@ -49,7 +49,8 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person,
+                           { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
