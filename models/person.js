60d3707ba0dd6af4a5e996f885baba87649370da
Kchou <K.chousos@protonmail.com>
2023-07-14 03:19:06 +0300

    3.20

--- models/person.js
+++ models/person.js
@@ -22,7 +22,12 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
+    minLength: 8,
+    validate: {
+      validator: v => {return /\d{2,3}-\d{6,}/.test(v)},
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
   }
 })
 
